578934e27a298cc24356576dbb0666a8f0804e22
Sergey Melnichenko <33039543+nuitmod@users.noreply.github.com>
2021-02-20 05:21:26 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>i_ion pri</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
+  
         <p>
           If you get lost, the{' '}
           <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
